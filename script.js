171fdc2ebae70e8a692e2ca6a40f1a69039f4d5c
Raphael_Gomes <115505663+Raphaelfnx@users.noreply.github.com>
2023-05-31 11:48:50 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ function beber(){
     copo.style.background = 'blue'
     ml7.style.background = 'blue'
     //ml7.style.background = 'blue'
-    hora1.innerHTML = `1 - Bebeu às ${hour}:${min}h`
+    hora1.innerHTML = `- Bebeu às ${hour}:${min}h`
     
 }
 
@@ -34,47 +34,47 @@ function beber2(){
     let copo2 = document.querySelector('.copo2')
     copo2.style.background = 'blue'
     ml6.style.background = 'blue'
-    hora2.innerHTML = `2 - Bebeu às ${hour}:${min}h`
+    hora2.innerHTML = `- Bebeu às ${hour}:${min}h`
 }
 
 function beber3(){
     let copo3 = document.querySelector('.copo3')
     copo3.style.background = 'blue'
     ml5.style.background = 'blue'
-    hora3.innerHTML = `3 - Bebeu às ${hour}:${min}h`
+    hora3.innerHTML = `- Bebeu às ${hour}:${min}h`
 }
 
 function beber4(){
     let copo4 = document.querySelector('.copo4')
     copo4.style.background = 'blue'
     ml4.style.background = 'blue'
-    hora4.innerHTML = `4 - Bebeu às ${hour}:${min}h`
+    hora4.innerHTML = `- Bebeu às ${hour}:${min}h`
 }
 
 function beber5(){
     let copo5 = document.querySelector('.copo5')
     copo5.style.background = 'blue'
     ml3.style.background = 'blue'
-    hora5.innerHTML = `5 - Bebeu às ${hour}:${min}h`
+    hora5.innerHTML = `- Bebeu às ${hour}:${min}h`
 }
 
 function beber6(){
     let copo6 = document.querySelector('.copo6')
     copo6.style.background = 'blue'
     ml2.style.background = 'blue'
-    hora6.innerHTML = `6 - Bebeu às ${hour}:${min}h`
+    hora6.innerHTML = `- Bebeu às ${hour}:${min}h`
 }
 
 function beber7(){
     let copo7 = document.querySelector('.copo7')
     copo7.style.background = 'blue'
     ml1.style.background = 'blue'
-    hora7.innerHTML = `7 - Bebeu às ${hour}:${min}h`
+    hora7.innerHTML = `- Bebeu às ${hour}:${min}h`
 }
 
 function beber8(){
     let copo8 = document.querySelector('.copo8')
     copo8.style.background = 'blue'
     ml0.style.background = 'blue'
-    hora8.innerHTML = `8 - Bebeu às ${hour}:${min}h`
+    hora8.innerHTML = `- Bebeu às ${hour}:${min}h`
 }
